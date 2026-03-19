3c8389d81e3b5eeabd57a3a175bd45db6f829142
kanieckinikodem2007 <kaniecki_nikodem@ckziu2.edu.pl>
2026-03-19 11:11:19 +0100

    Mój pierwszy kod

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-echo "# domka" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/kanieckinikodem2007/domka.git
-git push -u origin main
+public class Main {
+ public static void main(String[] args) {
+ System.out.println("Działa!");
+ }
+}
