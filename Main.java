410cf2779b120a68f06dcc97850865a62a878462
kanieckinikodem2007 <kaniecki_nikodem@ckziu2.edu.pl>
2026-03-19 11:07:29 +0100

    Mój pierwszy kod

---
+++ Main.java
@@ -0,0 +1,7 @@
+echo "# domka" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kanieckinikodem2007/domka.git
+git push -u origin main
